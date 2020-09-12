1d1c78edce5b04f214c475fa3d536e78058cb57e
Alber <34082417+AN0NIM0U5@users.noreply.github.com>
2020-09-12 18:11:03 +0200

    webView enable

--- app/src/main/java/com/example/mywebview/MainActivity.kt
+++ app/src/main/java/com/example/mywebview/MainActivity.kt
@@ -6,6 +6,7 @@ import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myWebView: WebView = findViewById(R.id.webview)
