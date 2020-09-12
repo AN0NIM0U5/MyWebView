da28755b76f022c368ee4dfb12428862f215e6ee
Alber <34082417+AN0NIM0U5@users.noreply.github.com>
2020-09-12 18:09:48 +0200

    First commit

--- app/src/main/java/com/example/mywebview/MainActivity.kt
+++ app/src/main/java/com/example/mywebview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mywebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("http://www.google.com")
     }
+
+
+
+
+
 }
